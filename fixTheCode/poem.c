ec4ba89d559bc7cb9beb9d7ab2b30edf739d773d
Hanna Shiferaw <hannashiferaw13@gmail.com>
2026-03-06 09:52:26 +0300

    simple I/O added

---
+++ fixTheCode/poem.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void){
+    printf("I WENT BACK\n");
+    printf("I went back\n");
+    printf("to my old home\n");
+    printf("and the furrow\n");
+    printf("of each year\n");
+    printf("plowed like\n"); 
+    printf("surf across\n");
+    printf("the place had\n");
+    printf("not washed \n");
+    printf("memory away\n");
+    return 0;
+}
